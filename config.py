f6f24502f3e44a9ed44ca15efe67842f90975514
NilsGuo <jysguobin@gmail.com>
2016-08-24 17:20:36 +0800

    qq mail

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class Config:
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Nils]'
-    FLASKY_MAIL_SENDER = '0307060@163.com'
+    FLASKY_MAIL_SENDER = 'nilsguo@qq.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 10
     FLASKY_FOLLOWERS_PER_PAGE = 50
