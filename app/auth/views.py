11a0f3ea111484eabf4f984228bf078d53ba465f
NilsGuo <jysguobin@gmail.com>
2016-08-25 20:27:19 +0800

    ch

--- app/auth/views.py
+++ app/auth/views.py
@@ -82,7 +82,7 @@ def resend_confirmation():
     token = current_user.generate_confirmation_token()
     send_email(current_user.email, 'Confirm Your Account',
                'auth/email/confirm', user=current_user, token=token)
-    flash('A new confirmation email has been sent to you by email.')
+    flash(u'一封新的确认邮件已发送到你的邮箱')
     return redirect(url_for('main.index'))
 
 
